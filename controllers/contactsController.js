fea2cd86ca19e122f0e88d506e90cccbd65d351f
SilviaCastagnino <sylvia19976@gmail.com>
2026-01-16 15:23:06 +0100

    W01 Project: Contacts Part 1

---
+++ controllers/contactsController.js
@@ -0,0 +1,24 @@
+const mongodb = require('../data/database');
+const objectId = require('mongodb').ObjectId;
+
+const getAllContacts = async (req, res) => {
+  const result = await mongodb.getDatabase().db('contacts').collection('contacts').find();
+  result.toArray().then((contacts) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(contacts);
+  });
+};
+
+const getContactById = async (req, res) => {
+  const contactId = new objectId(req.params.id);
+  const result = await mongodb.getDatabase().db('contacts').collection('contacts').find({ _id: contactId });
+  result.toArray().then((contacts) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(contacts[0]);
+  });
+};
+
+module.exports = {
+  getAllContacts,
+  getContactById
+};
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongodb = require('./data/database.js');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(PORT, () => { console.log(`Running on port ${PORT}`) });
+mongodb.initDb((err) => {
+  if (err) {
+    console.log('Failed to connect to database');
+  } else {
+    app.listen(PORT, () => {
+      console.log(`Database is listening and node is running on port ${PORT}`)
+    });
+  }
+});
